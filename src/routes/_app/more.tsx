00511fa4e9d0b3a69f94aabf207a5fc290996100
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2026-03-01 20:12:54 +0100

    fix: redirect to home if on more page on desktop

--- src/routes/_app/more.tsx
+++ src/routes/_app/more.tsx
@@ -1,9 +1,21 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet, useRouter } from "@tanstack/react-router";
+import { useIsDesktop } from "../../hooks/breakpoint";
 
 export const Route = createFileRoute("/_app/more")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const isDesktop = useIsDesktop();
+  const router = useRouter();
+
+  if (isDesktop) {
+    // This should really be done in beforeLoad, but we can't use hooks there.
+    router.navigate({
+      to: "/",
+      replace: true,
+    });
+  }
+
   return <Outlet />;
 }
