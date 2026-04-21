2f6a2260d801678e8ac9ef5e48103b4842880b68
Malcolm Nihlén <malcolm.nihlen@gmail.com>
2026-04-21 20:40:04 +0200

    fix: remove placeholder home page

--- src/routes/_app/index.tsx
+++ src/routes/_app/index.tsx
@@ -1,18 +1,7 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_app/")({
-  component: Index,
+  loader: () => {
+    throw redirect({ to: "/home", replace: true });
+  },
 });
-
-function Index() {
-  return (
-    <div className="p-2">
-      <p>
-        This page is rendered by the main app. It's not part of any
-        microfrontend.
-      </p>
-
-      <p>Dummy change!</p>
-    </div>
-  );
-}
